80dc59b506bc9203490c9a85906047d7f99da82f
Sila Rim <rimsila.itc@gmail.com>
2020-11-22 03:11:58 +0700

    update config

--- .umirc.js
+++ .umirc.js
@@ -36,7 +36,7 @@ export default {
     '@s-content-margin': '50px',
   },
   base: '/next-dev',
-  publicPath: '/next-dev',
+  publicPath: '/next-dev/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   extraBabelPlugins: [
