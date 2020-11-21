1b24eb49e8855a5577b8f18799dc8de5f9f96351
Sila Rim <rimsila.itc@gmail.com>
2020-11-22 03:16:12 +0700

    Updates

--- packages__component__demos__icon-font__base.da252e55.async.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[11],{I3ON:function(t,e,n){"use strict";n.r(e);var o=n("q1tI"),c=n.n(o),a=n("KiPk");Object(a["e"])({iconFontUrl:["//at.alicdn.com/t/font_1830224_kbe7ufh34m.js"]}),e["default"]=()=>c.a.createElement(a["a"],{style:{fontSize:32},type:"icon-wt-logo"})}}]);
